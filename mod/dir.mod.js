7d10743b1df0f299593321c4ca4d97c93ef28b65
F1LT3R <al@pwn.io>
2016-10-24 15:20:25 -0400

    Adding plugin arch.

--- mod/dir.mod.js
+++ mod/dir.mod.js
@@ -1,1 +1,20 @@
-module.exports = () => {};
+const Markserv = require('markserv-cli');
+
+Markserv.plugin.modifier(module, (Markconf, template) => {
+  return requestPath => {
+    return new Promise((resolve, reject) => {
+
+      const result = requestPath;
+
+      // Pass Back to HTTP Request Handler or HTTP Exporter
+      const payload = {
+        statusCode: 200,
+        contentType: 'text/html',
+        data: result
+      };
+
+      // return payload;
+      resolve(payload);
+    });
+  };
+});
